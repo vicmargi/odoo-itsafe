4c7ffb2e538c0661bbd4a935a0cf5b5161751e1c
vicmargi <martinez17v@gmail.com>
2025-04-06 23:25:55 -0500

    modificado el manifest name company

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "My ITSAFE S.A.",
     'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
