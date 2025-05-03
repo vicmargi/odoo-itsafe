94da75c14a86f4f2c8909ba14f87863efd4292dd
vicmargi <martinez17v@gmail.com>
2025-05-03 11:43:44 -0500

    second itsafe commit

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 Long description of module's purpose
     """,
 
-    'author': "My ITSAFE Sebastian y victor",
+    'author': "My ITSAFE Sebastian y victor 2025",
     'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
