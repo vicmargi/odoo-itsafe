6efdc8bf897056baa48ad635f01ec5fbb9c49a48
vicmargi <martinez17v@gmail.com>
2025-05-03 11:23:47 -0500

    firts sebastian

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 Long description of module's purpose
     """,
 
-    'author': "My ITSAFE S.A.",
+    'author': "My ITSAFE Sebastian",
     'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
